f763c4889f3a0a6d6417b133c7be46998b94d679
chetan dhargalkar <chetan.dmc@gmail.com>
2020-08-29 10:07:54 +0530

    Update forms.js

--- assets/js/form/forms.js
+++ assets/js/form/forms.js
@@ -129,7 +129,7 @@ $document.on('ready', function () {
 				$(form).ajaxSubmit({
 					type: "POST",
 					data: $(form).serialize(),
-					url: "assets/js/form/process-question.php",
+					url: "/assets/js/form/process-question.php",
 					success: function success() {
 						$('.successform', $questionForm).fadeIn();
 						$questionForm.get(0).reset();
