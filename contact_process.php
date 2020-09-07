237d118ab04b683bc7db5f0c71eec3c3f2ee4ff1
chetan dhargalkar <chetan.dmc@gmail.com>
2020-09-07 22:35:58 +0530

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -7,7 +7,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
         $name = strip_tags(trim($_POST["name"]));
-                $name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $name = str_replace(array("\r","\n"),array(" "," "),$name);
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
         // $cont_subject = trim($_POST["subject"]);
         $message = trim($_POST["message"]);
